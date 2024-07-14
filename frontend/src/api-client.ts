566001dbcea54fda81762d735e08e50d68947f64
Ahmed <fyotol@gmail.com>
2024-07-14 10:21:23 -0700

    Add: add shared folder in backend and add my-hotels view page and test it | 14/7 | 1h

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,6 +1,6 @@
-import { HotelType } from "../../backend/src/shared/types.ts";
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
+import { HotelType } from "./shared/types";
 
 const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL as string) || "";
 
