2b8c090cae8fd74854c1d715fbbdeee3baa5af7c
Ahmed <fyotol@gmail.com>
2024-07-17 12:38:54 -0700

    Add: add detail Page and Test | 2h | 7/17

--- e2e-test/tests/search-hotels.spec.ts
+++ e2e-test/tests/search-hotels.spec.ts
@@ -21,3 +21,14 @@ test("Should show hotel search results", async ({ page }) => {
   await expect(page.getByText("Hotels Found in test")).toBeVisible();
   await expect(page.getByText("Hotel Mode").first()).toBeVisible();
 });
+
+test("Should show hotel detail", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("Where are you going?").fill("test");
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await page.getByText("Hotel Mode").first().click();
+  await expect(page).toHaveURL(/detail/);
+  await expect(page.getByRole("button", { name: "Book Now" })).toBeVisible();
+});
